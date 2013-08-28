9aef4b9ad9617d4e3acea0ececce1a95ef736748
Sumit Kumar Singh <sumitkumarsingh@Sumits-Mac.local>
2013-08-28 09:54:45 +0530

    Initial Commit

---
+++ Fitzi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Fitzi
+//
+//  Created by Sumit Kumar Singh on 28/08/13.
+//  Copyright (c) 2013 Gaurav Bhardwaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Fitzi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Fitzi
+//
+//  Created by Sumit Kumar Singh on 28/08/13.
+//  Copyright (c) 2013 Gaurav Bhardwaj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
